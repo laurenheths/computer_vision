b2e74338905795b78d2f071ba2eef09ee7b7338b
Lauren Hetherington <lauren@goodcall.biz>
2023-05-22 17:55:17 +0100

    Final working rock, paper and scissors game

--- converted_keras/practice_cv.py
+++ converted_keras/practice_cv.py
@@ -10,6 +10,7 @@ def get_computer_choice():
     return computer_choice
 
 def get_prediction():
+    global cap
     model = load_model('keras_model.h5')
     cap = cv2.VideoCapture(0)
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
@@ -20,57 +21,58 @@ def get_prediction():
         image_np = np.array(resized_frame)
         normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
         data[0] = normalized_image
+        cv2.imshow('frame', frame)
         prediction = model.predict(data)
         user_choice = choices_list[prediction.argmax()]
-        cap.release()
-        cv2.destroyAllWindows()
         return user_choice
 
+def countdown():
+    countdown = 5
+    print("\n Preapre to show your choice...")
+    while countdown > 0 :
+        print(f"{countdown}")
+        cv2.waitKey(1000)
+        countdown -= 1
+    print("\nShow your choice!")     
+
 def play_game():
+    
+    global computer_wins
+    global user_wins
     computer_wins = 0
     user_wins = 0
-    round_count = 0
-    cap = cv2.VideoCapture(0)
-    while computer_wins < 3 and user_wins < 3:
-        print("round start")
-        round_count += 1
-        count = 3
-        start_time = time.time()
-        while True:
-            time_left = time.time() - start_time
-            if time_left <= count:
-                print(f"{count}...")
-                count -= 1
-                ret, frame = cap.read() 
-                cv2.imshow('frame', frame)  
-                cv2.waitKey(1000) 
-                time.sleep(1)
-            else:
-                break
-        cap.release() 
+    round_count = 1
 
-        computer_choice = get_computer_choice()
-        user_choice = get_prediction()
-        winner = get_winner(user_choice, computer_choice)
-        print(f"You chose {user_choice}. Computer chose {computer_choice}.")
-        if winner == "Computer":
-            computer_wins += 1
-            print("Computer wins this round!")
-        elif winner == "User":
-            user_wins += 1
-            print("You win this round!")
-        elif winner == "Tie":
-            print("This round is a tie.")
+
+    while True:
+        if computer_wins == 3:
+            print('Game over! Computer won!')
+            cap.release()
+            cv2.destroyAllWindows()
+            break
+        elif user_wins == 3:
+            print('Game Over! You won!')
+            cap.release()
+            cv2.destroyAllWindows()
+            break
         else:
-            print("Invalid input")
-        print(f"User score: {user_wins}. Computer score: {computer_wins}.")
-    
-    if computer_wins == 3:
-        print("Computer wins the game!")
-    elif user_wins == 3:
-        print("You win the game!")
-
-    cv2.destroyAllWindows()
+            print(f"Starting round {round_count}!")
+            countdown()
+            user_choice = get_prediction()
+            computer_choice = get_computer_choice()
+            print(f"You chose {user_choice}. Computer chose {computer_choice}.")
+            winner = get_winner(user_choice, computer_choice)
+            if winner == "Computer":
+                computer_wins += 1
+                print("Computer wins this round!")
+            elif winner == "User":
+                user_wins += 1
+                print("You win this round!")
+            elif winner == "Tie":
+                print("This round is a tie.")
+            else:
+                print("Invalid input")
+            round_count += 1
 
 def get_winner(user_choice, computer_choice):
     if (computer_choice == 'rock' and user_choice == 'scissors') or (computer_choice == "scissors" and user_choice == 'paper') or (computer_choice == 'paper' and user_choice == 'rock'): 
