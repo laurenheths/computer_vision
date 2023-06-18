a50bd44e27adef9e9d478c41867a41f7a968af85
Lauren Hetherington <lauren@goodcall.biz>
2023-06-18 13:33:02 +0100

    Edited Rock, paper scissors code after my review

--- converted_keras/camera_rps.py
+++ converted_keras/camera_rps.py
@@ -9,65 +9,67 @@ def get_computer_choice():
     computer_choice = random.choice(game_options)
     return computer_choice
 
+def countdown(seconds):
+    for i in range(seconds, 0, -1):
+        print(f"{i}...")
+        time.sleep(1)
+
 def get_prediction():
     model = load_model('keras_model.h5')
     cap = cv2.VideoCapture(0)
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
     choices_list = ['rock', 'paper', 'scissors', 'nothing']
-    computer_wins = 0
-    user_wins = 0
     round_count = 0
-    while computer_wins < 3 and user_wins < 3:
+    while True:
         round_count += 1
-        count = 3
-        start_time = time.time()
+        countdown(3)
         while True:
-            time_left = time.time() - start_time
-            if time_left <= count:
-                print(f"{count}...")
-                count -= 1
-                time.sleep(1)
-            else:
-                break
-            while True:
-                ret, frame = cap.read()
-                resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
-                image_np = np.array(resized_frame)
-                normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
-                data[0] = normalized_image
-                prediction = model.predict(data)
-                user_choice = choices_list[prediction.argmax()]
-                computer_choice = get_computer_choice()
-                winner = get_winner(user_choice, computer_choice)
-                print(f"You chose {user_choice}. Computer chose {computer_choice}.")
-                if winner == "Computer":
-                    computer_wins += 1
-                    print("Computer wins this round!")
-                elif winner == "User":
-                    user_wins += 1
-                    print("You win this round!")
-                elif winner == "Tie":
-                    print("This round is a tie.")
-                else:
-                    print("Invalid input")
-                print(f"User score: {user_wins}. Computer score: {computer_wins}.")
-                cv2.imshow('frame', frame)
-                    # Press q to close the window
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
-                if computer_wins == 3 or user_wins == 3:
-                    break
-
-            if computer_wins == 3:
-                print("Computer wins!")
-                break
-            elif user_wins == 3:
-                print("User wins!")
+            ret, frame = cap.read()
+            resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+            image_np = np.array(resized_frame)
+            normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
+            data[0] = normalized_image
+            prediction = model.predict(data)
+            user_choice = choices_list[prediction.argmax()]
+            computer_choice = get_computer_choice()
+            cv2.imshow('frame', frame)
+                # Press q to close the window
+            if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
         cap.release()
         cv2.destroyAllWindows()
-        return
+        return user_choice, computer_choice
+    
+def play_game():
+    computer_wins = 0
+    user_wins = 0
+    round_count = 0
+    while True:
+        round_count += 1
+        countdown(3)
+        user_choice, computer_choice = get_prediction()
+        winner = get_winner(user_choice, computer_choice)
+        print(f"You chose {user_choice}. Computer chose {computer_choice}.")
+        if winner == "Computer":
+            computer_wins += 1
+            print("Computer wins this round!")
+        elif winner == "User":
+            user_wins += 1
+            print("You win this round!")
+        elif winner == "Tie":
+            print("This round is a tie.")
+        else:
+            print("Invalid input")
+        print(f"User score: {user_wins}. Computer score: {computer_wins}.")
+
+        
+        if computer_wins == 3:
+            print("Computer wins!")
+        elif user_wins == 3: 
+            print("User wins!")
+
+    
 
 def get_winner(user_choice, computer_choice):
     if (computer_choice == 'rock' and user_choice == 'scissors') or (computer_choice == "scissors" and user_choice == 'paper') or (computer_choice == 'paper' and user_choice == 'rock'): 
@@ -79,4 +81,4 @@ def get_winner(user_choice, computer_choice):
     else:
         return "invalid input"
 
-get_prediction()
+play_game()
